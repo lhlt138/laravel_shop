8385dd80dc91002313531307d77c45f4606b743d
jianfa <1611397198@qq.com>
2019-08-03 20:22:20 +0800

    first upload

---
+++ database/migrations/2019_08_03_120623_create_talbe_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTalbeUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //创建表 user是你的表名 
+        Schema::create('users', function (Blueprint $table) {
+            //increments string timestamps 类型 
+            $table->increments('id');
+            $table->string('name');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('user');
+    }
+}
